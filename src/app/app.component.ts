2fd717f9266e21b38ba07f1265e7c3e226b0bc8a
Abdelrhman <abdoalaa2146@gmail.com>
2025-10-26 15:22:20 +0300

    init svg starting animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { HeaderComponent } from './core/layout/header/header.component';
 import { ToastModule } from 'primeng/toast';
 import { TourGuideService } from './core/services/tour-guide.service';
 import Shepherd from 'shepherd.js';
-import { GuidelinesStepsComponent } from "./core/layout/guidelines-steps/guidelines-steps.component";
+import { GuidelinesStepsComponent } from './core/layout/guidelines-steps/guidelines-steps.component';
 interface DialogStep {
   id: string;
   imageUrl: string;
@@ -18,11 +18,31 @@ interface DialogStep {
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, HeaderComponent, ToastModule, GuidelinesStepsComponent],
+  imports: [
+    RouterOutlet,
+    SidebarComponent,
+    HeaderComponent,
+    ToastModule,
+    GuidelinesStepsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Tredo task';
+  ngAfterViewInit() {
+    const obj = document.querySelector(
+      '.animated-bracket'
+    ) as HTMLObjectElement;
+    obj.addEventListener('load', () => {
+      const svgDoc = obj.contentDocument;
+      const snakeTop = svgDoc?.querySelector('.snake-top');
+      const snakeBottom = svgDoc?.querySelector('.snake-bottom');
 
+      // Example: animate with CSS or Anime.js
+      svgDoc?.querySelectorAll('.snake').forEach((el) => {
+        el.classList.add('animate');
+      });
+    });
+  }
 }
